c9964c85ee4b0af1045ddab6324238f2a176187a
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-24 15:12:55 +0530

    array

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var no = 123456;
 var no2 = 123.85569;
 console.log(no.toExponential(2));
@@ -16,3 +25,34 @@ console.log(str1.concat(str2));
 console.log(str2.indexOf('c'));
 var x = true;
 console.log(x);
+var arr1;
+arr1 = [1, 2, 3, 4];
+console.log("Array[0]: " + arr1[0]);
+console.log("Array[1]: " + arr1[1]);
+var mArray = [[1, 2, 3], [4, 5, 6]];
+console.log(mArray[0][0]);
+console.log(mArray[0][1]);
+console.log(mArray[0][2]);
+console.log();
+console.log(mArray[1][0]);
+console.log(mArray[1][1]);
+console.log(mArray[1][2]);
+// Passing Arrays to Functions
+var arry = new Array("Jatin", "2022", "Angular", "typescript");
+function display(arr_values) {
+    for (var i = 0; i < arr_values.length; i++) {
+        console.log(arr_values[i]);
+    }
+}
+display(arry);
+// Spread Operator : is used to initialize arrays and objects from another array 
+var arry1 = [1, 2, 3];
+var arry2 = [4, 5, 6];
+var copyArray = __spreadArray([], arry1, true);
+console.log("CopiedArray:" + copyArray);
+// // create new array from existing array with more elements
+var newArray = __spreadArray(__spreadArray([], arry1, true), [7, 8], false);
+console.log("NewArray:" + newArray);
+// // create array by merging two arrays
+var mergedArray = __spreadArray(__spreadArray([], arr1, true), arry2, true);
+console.log("MergedArray:" + mergedArray);
--- datatypes.ts
+++ datatypes.ts
@@ -18,3 +18,51 @@ console.log(str1.concat(str2));
 console.log(str2.indexOf('c'));
 let x : boolean = true;
 console.log(x);
+
+
+let arr1:number[];
+arr1 = [1,2,3,4];
+ console.log("Array[0]: " +arr1[0]);
+console.log("Array[1]: " +arr1[1]);
+
+var mArray:number[][] = [[1,2,3],[4,5,6]];
+
+console.log(mArray[0][0]);
+ console.log(mArray[0][1]);
+ console.log(mArray[0][2]);
+  console.log();
+ console.log(mArray[1][0]);
+ console.log(mArray[1][1]);
+console.log(mArray[1][2]);
+
+
+// Passing Arrays to Functions
+
+ let arry:string[] = new Array("Jatin","2022","Angular","typescript");
+
+
+function display(arr_values:string[]){
+ for (var i = 0; i<arr_values.length; i++) {
+     console.log(arr_values[i]);
+} }
+display(arry);
+
+
+// Spread Operator : is used to initialize arrays and objects from another array 
+
+ let arry1 = [1,2,3];
+let arry2 = [4,5,6];
+
+
+
+ let copyArray = [...arry1];
+console.log("CopiedArray:" +copyArray);
+
+// // create new array from existing array with more elements
+ let newArray = [...arry1, 7, 8];
+ console.log("NewArray:" +newArray);
+
+// // create array by merging two arrays
+
+let mergedArray = [...arr1, ...arry2];
+console.log("MergedArray:" +mergedArray);
