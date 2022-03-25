bbfb18d444bdf735ef4d0e94606ad0bca0056a34
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-25 15:05:58 +0530

    astype

---
+++ interface.js
@@ -0,0 +1,5 @@
+//as Type
+var kv1 = { key: 1, value: "A" };
+var kv2 = { key: 2, value: "B" };
+console.log(kv1);
+console.log(kv2);
---
+++ interface.ts
@@ -0,0 +1,12 @@
+//as Type
+
+interface KeyPair {
+    key: number;
+    value: string;
+}
+
+let kv1: KeyPair = { key:1, value:"A" }; 
+
+let kv2: KeyPair = { key:2, value:"B" }; 
+console.log(kv1);
+console.log(kv2);
