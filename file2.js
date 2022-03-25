a044adc131a0381fd68eff6738ccb3c5ea3b05b0
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-25 16:38:35 +0530

    namespace

--- file2.js
+++ file2.js
@@ -3,3 +3,21 @@ exports.__esModule = true;
 var file1_1 = require("./file1");
 var ob = new file1_1.ex();
 ob.display();
+var upper;
+(function (upper) {
+    function ToCapital(str) {
+        return str.toUpperCase();
+    }
+    upper.ToCapital = ToCapital;
+})(upper || (upper = {}));
+var namespc = /** @class */ (function () {
+    function namespc() {
+        this.t1 = upper.ToCapital("welcome");
+    }
+    namespc.prototype.disp = function () {
+        console.log(this.t1);
+    };
+    return namespc;
+}());
+var n = new namespc();
+n.disp();
--- file2.ts
+++ file2.ts
@@ -2,3 +2,21 @@ import { ex } from "./file1";
 
 const ob = new ex();
 ob.display();
+
+namespace upper {
+
+    export function ToCapital(str: string): string {
+        return str.toUpperCase();
+    }
+}
+class namespc {
+    
+    t1 = upper.ToCapital("welcome");
+    disp()
+    {
+        console.log(this.t1);
+    }
+}
+
+const n = new namespc();
+n.disp();
