ba5e3002b18c5a00e259122de583bbc63421b12a
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-25 14:33:59 +0530

    empInterface

--- emp.ts
+++ emp.ts
@@ -2,3 +2,11 @@ interface Project{
     id : number;
     name : string;
 }
+
+interface emp {
+    id : number
+    name : string
+    isActive : boolean
+    designation ?: string
+  project ?: Array<Project>
+}
