9944b9d43ce51415be6254261aef5976e7053cad
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-25 16:07:33 +0530

    abstarct

--- class.js
+++ class.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Employee = /** @class */ (function () {
     function Employee(code, name) {
         this.empName = name;
@@ -11,3 +26,28 @@ var Employee = /** @class */ (function () {
 }());
 var b1 = new Employee(101, "Raj");
 b1.get();
+//abstract
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name1 = name;
+    }
+    return Person;
+}());
+var emp1 = /** @class */ (function (_super) {
+    __extends(emp1, _super);
+    function emp1(id, name) {
+        var _this = _super.call(this, name) || this;
+        _this.id = id;
+        return _this;
+    }
+    emp1.prototype.display = function () {
+        return new emp1(101, "Pooja");
+    };
+    emp1.prototype.get = function () {
+        console.log(this.id);
+        console.log(this.name1);
+    };
+    return emp1;
+}(Person));
+var e = new emp1(102, "Diya");
+e.get();
--- class.ts
+++ class.ts
@@ -15,3 +15,41 @@ class Employee {
 
 const b1 = new Employee(101, "Raj");
 b1.get();
+
+
+//abstract
+
+abstract class Person
+{
+    name1 : string;
+    constructor(name:string)
+    {
+        this.name1 = name;
+    }
+
+    abstract display() : emp1;
+}
+
+
+class emp1 extends Person{
+    id : number;
+    constructor(id:number,name:string)
+    {
+        super(name);
+        this.id = id;
+    }
+
+ display():emp1 {
+    return new emp1(101,"Pooja");
+    
+ }   
+
+ get()
+ {
+     console.log(this.id);
+     console.log(this.name1);
+ }
+}
+
+const e = new emp1(102 , "Diya");
+e.get();
