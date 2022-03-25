6a99f0d66e68b934066357d83ddcbfbb630a63e1
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-25 14:35:10 +0530

    EmpArrClass

--- emp.ts
+++ emp.ts
@@ -10,3 +10,68 @@ interface emp {
     designation ?: string
   project ?: Array<Project>
 }
+
+
+
+class EmpArrr{
+    public emparr : Array<emp> =[
+        {
+            id : 1,
+            name:"pooja",
+            isActive:true,
+            designation :"manager",
+            
+            project : [
+                {
+                    id:101,
+                    name:"angular"
+                },
+                {
+                    id:102,
+                    name:"delta"
+                }
+            ]
+    
+        },
+    
+    
+        {
+            id : 2,
+            name:"Shreyas",
+            isActive:true,
+            designation :"developer",
+    
+            project : [
+                {
+                    id:303,
+                    name:"hp"
+                }
+            ]
+    
+        },
+    
+        {
+            id : 3,
+            name:"Shubham",
+            isActive:true,
+            designation :"QA",
+            
+            project : [
+                {
+                    id:201,
+                    name:"TSG"
+                },
+                {
+                    id:202,
+                    name:"DBA"
+                }
+            ]
+    
+        },
+    
+    ];
+    
+    
+    }
+    
+    
