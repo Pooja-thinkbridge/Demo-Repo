9b77252b7a70b9c7b74a1932d607c399cf905e36
Pooja-thinkbridge <99236407+Pooja-thinkbridge@users.noreply.github.com>
2022-03-31 15:49:25 +0530

    op

---
+++ rxjs/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OperatorsComponent } from './operators/operators.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OperatorsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
